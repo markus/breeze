497badcab06563adb72d3bec3d4a2e481243ace9
Markus Bengts <markus.bengts@gmail.com>
2011-02-13 18:21:57 +0100

    thor options can be updated

--- lib/breeze/veur.rb
+++ lib/breeze/veur.rb
@@ -8,10 +8,18 @@ module Breeze
 
     private
 
+    # Thor freezes the options (don't understand why)
+    def options
+      @unfrozen_options ||= super.dup
+    end
+
+    # yes? in thor cannot be accepted with enter
     def accept?(question)
       ! (ask("#{question} [YES/no] >") =~ /n/i)
     end
 
+    # Print a table with a title and a top border of matching width.
+    # The first row must contain column titles, less than 2 rows is not printed.
     def report(title, table)
       table = capture_table(table)
       title = "=== #{title} "
@@ -20,9 +28,10 @@ module Breeze
       puts table
     end
 
+    # capture table in order to determine it's width
     def capture_table(table)
       return 'none' if table.size == 1 # the first row is for column titles
-      $stdout = StringIO.new  # capture output in order to adjust title width
+      $stdout = StringIO.new  # start capturing the output
       print_table(table)
       output = $stdout
       $stdout = STDOUT        # restore normal output
