930c8dc88a23edc53222cfa331502bdad2acceff
Markus Bengts <markus.bengts@gmail.com>
2011-03-05 12:22:19 +0100

    reports: name first then id

--- lib/breeze/tasks/list.rb
+++ lib/breeze/tasks/list.rb
@@ -13,18 +13,18 @@ module Breeze
     desc :images, 'Describe machine images owned by Breeze::CONFIGURATION[:image_owner]'
     def images
       report 'MACHINE IMAGES',
-        ['Image ID', 'Owner', 'Name or Location', 'Image Type', 'Public'],
+        ['Name or Location', 'Image ID', 'Owner', 'Image Type', 'Public'],
         fog.images.all('Owner' => Breeze::CONFIGURATION[:image_owner]).map{ |i|
-          [i.id, i.owner_id, i.name||i.location, i.full_type, i.is_public]
+          [i.name||i.location, i.id, i.owner_id, i.full_type, i.is_public]
         }
     end
 
     desc :servers, 'Describe server instances'
     def servers
       report "SERVER INSTANCES",
-        ['Name', 'IP Address', 'Instance ID', 'Image ID', 'Instance Type', 'Availability Zone', 'State'],
+        ['Name', 'Instance ID', 'IP Address', 'Image ID', 'Instance Type', 'Availability Zone', 'State'],
         fog.servers.map { |i|
-          [i.name, i.ip_address, i.id, i.image_id, i.flavor_id, i.availability_zone, i.state]
+          [i.name, i.id, i.ip_address, i.image_id, i.flavor_id, i.availability_zone, i.state]
         }
     end
 
