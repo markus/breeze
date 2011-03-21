d8a52bb37d09dc19e9251ea3c43601f456925ab4
Markus Bengts <markus.bengts@gmail.com>
2011-03-21 13:00:38 +0100

    [server:image:create] work around some issues with fog 0.7.0

--- lib/breeze/tasks/server/image.rb
+++ lib/breeze/tasks/server/image.rb
@@ -8,17 +8,22 @@ module Breeze
                    'stop the server, create a new machine image as a snapshot of the root device, and destroy the server.'
     method_options CONFIGURATION[:default_server_options].merge(CONFIGURATION[:create_image_options])
     def create
-      options[:block_device_mapping] = [{:device_name => '/dev/sda1', :ebs_volume_size => options.delete(:root_device_size)}]
+      # The commented lines used to work before fog 0.7.0.
+      # options[:block_device_mapping] = [{:device_name => '/dev/sda1', :ebs_volume_size => options.delete(:root_device_size)}]
+      options[:block_device_mapping] = [{'DeviceName' => '/dev/sda1', 'Ebs.VolumeSize' => options.delete(:root_device_size)}]
       server = create_server(options)
       prepare_private_image(server.public_ip_address)
       print('Stopping the server before saving a snapshot')
       server.stop
       wait_until('stopped!') { server.stopped? }
       thor('list:images')
-      image = fog.images.create(:name => ask('Image name >'), :instance_id => server.id)
+      puts('===== Old server images are listed above. Give a name to the new image. =====')
+      # image = fog.images.create(:name => ask('Image name >'), :instance_id => server.id)
+      fog.create_image(server.id, ask('Image name >'), '')
       server.destroy
       puts
-      puts("===> Created image #{image.id} and terminated temporary server #{server.id}.")
+      # puts("===> Created image #{image.id} and terminated temporary server #{server.id}.")
+      puts("===> Created a new server image and terminated temporary server #{server.id}.")
       puts
       puts("NOTICE: it may take a while before the new image shows up in list:images")
     end
